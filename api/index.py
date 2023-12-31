0c11a026716b0a078b2c5f943f4f8ce38359eddf
Tomás Phillips <tomas.phillips@Tomass-MacBook-Pro.local>
2023-12-31 13:29:17 -0500

    remove handler

--- api/index.py
+++ api/index.py
@@ -85,15 +85,6 @@ client = GithubClient()
 
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write("Hello, world!".encode("utf-8"))
-        return
-
-
-class SMSHandler(BaseHTTPRequestHandler):
     client = GithubClient()
 
     def do_POST(self):
